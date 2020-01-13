f0cd02f5baa25ed11adbc5d54842a446e43679e2
Filip Johannesson <filip.johannesson@neodev.se>
2020-01-13 22:36:20 +0100

    Fix for pause-issue

--- waveform.py
+++ waveform.py
@@ -51,25 +51,40 @@ class PlayHead(Widget):
 		newPos_x = self.playHead_time * self.visual_audio_rate + self.start_x
 		self.rect.pos = (newPos_x, 100.0)
 
-class SoundVisualizerWidget(Widget):
+class SoundVisualizer(Widget):
 	speed = NumericProperty(0)
 
 	def update_playhead(self, dt):
-		self.pH.playHead_time = self.audioToPlay.get_pos()
+		sound_time = self.audioToPlay.get_pos()
+		scroll_x = 1.0 / self.pH.song_length * sound_time
+
+		if self.hasPaused:
+			if sound_time == 0.0:
+				self.parent.scroll_x = self.old_scroll#pdb.set_trace()
+				print("old_scroll: {}, scroll_x {}, scrollbar {}, playHead_audio {}".format( self.old_scroll, scroll_x, self.parent.scroll_x, self.pH.playHead_time))
+			self.hasPaused = False
+		else:
+			self.pH.playHead_time = sound_time
+			self.parent.scroll_x = scroll_x
+
 		self.pH.move()
+		self.old_scroll = 1.0 / self.pH.song_length * self.pH.playHead_time
 
 	def __init__(self,
 				audioVisualSegment,
 				audioSound,
 				BARS = 5000,
 				BAR_HEIGHT = 300,
-				BAR_WIDTH = 1,
-				LINE_WIDTH = 0,
+				BAR_WIDTH = 10,
+				LINE_WIDTH = 1,
 				**kwargs):
-		super(SoundVisualizerWidget, self).__init__(**kwargs)
+		super(SoundVisualizer, self).__init__(**kwargs)
 		self.audioVizData = audioVisualSegment
 		self.audioToPlay = audioSound
 		self.data = np.fromstring(self.audioVizData._data, np.int16)
+
+		self.old_scroll = 0.0
+		self.hasPaused = False
 
 		# Set some intitial values
 		self.BARS = BARS  #One bar per second
@@ -77,7 +92,7 @@ class SoundVisualizerWidget(Widget):
 		self.BAR_WIDTH = BAR_WIDTH
 		self.LINE_WIDTH = LINE_WIDTH
 
-		# set widget sizes and size hint
+		## set widget sizes and size hint
 		self.size = (self.BARS * (self.BAR_WIDTH + self.LINE_WIDTH), 600)
 		self.size_hint_x = None
 
@@ -113,35 +128,42 @@ class SoundVisualizerWidget(Widget):
 			self.current_x = self.current_x + self.LINE_WIDTH + self.BAR_WIDTH
 
 		with self.canvas:
-			self.pH = PlayHead(len(self.audioVizData)/1000,self.BARS * (self.LINE_WIDTH + self.BAR_WIDTH), 1)
+			self.pH = PlayHead(len(self.audioVizData)/1000,len(self.max_array) * (self.LINE_WIDTH + self.BAR_WIDTH), 1)
+
 
 	def schedule_animation(self, sound):
 		event = Clock.schedule_interval(self.update_playhead, 1 / 30. )#/ 30.)
+		#pdb.set_trace()
 
 	def unSchedule_animation(self, sound):
 		Clock.unschedule(self.update_playhead)
+		self.hasPaused = True
+		#pdb.set_trace()
+
+class ScrollableSoundVizualizer(ScrollView):
+
+	def __init__(self, audioViz, sound,**kwargs):
+		super(ScrollableSoundVizualizer, self).__init__(**kwargs)
+		self.visualizer = SoundVisualizer(audioViz, sound)
+		self.visualizer.preprocess_data()
+		self.visualizer.start(1, 300.0)
+		self.add_widget(self.visualizer)
+		self.size_hint_x = None
+		self.size = Window.size
+		self.do_scroll_x = True
+
+		sound.bind(on_play = self.visualizer.schedule_animation)
+		sound.bind(on_stop = self.visualizer.unSchedule_animation)
 
 class SoundVisualizerApp(App):
 	def build(self):
-		# Set WindowSize
-		Config.set('graphics', 'width', '1000')
-		Config.set('graphics', 'height', '600')
 
 		#Read and parse audio data
 		src= "./AdhesiveWombat - Anthem.mp3"#"./test.mp3"
 		audio = AudioSegment.from_file(src)
 		self.sound = SoundLoader.load(src)
+		sView = ScrollableSoundVizualizer(audio, self.sound)
 
-		sWig = SoundVisualizerWidget(audio, self.sound)
-		sWig.preprocess_data()
-		sWig.start(1, 300.0)
-
-		sView = ScrollView(size_hint=(None, 1), size=(1000, 600))
-		sView.add_widget(sWig)
-		sView.do_scroll_x = True
-
-		self.sound.bind(on_play = sWig.schedule_animation)
-		self.sound.bind(on_stop = sWig.unSchedule_animation)
 		self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
 		self._keyboard.bind(on_key_down = self._on_keyboard_toggle)
 
